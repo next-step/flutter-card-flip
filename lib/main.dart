422027fd86f3c7d9af5d960ad523fab298ba5174
jyb <jyb0823@gmail.com>
2022-07-22 23:49:41 +0900

    game 1차 완성. 버그는 있음

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flip_card/flip_card.dart';
+import 'package:flip_card_game/asset_name.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,21 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +31,141 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _imageNames = [
+    AssetImageName.orange,
+    AssetImageName.banana,
+    AssetImageName.apple,
+    AssetImageName.strawberry,
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final List<String> _randomImageNames = [];
+  final List<GlobalKey<FlipCardState>> _cardKeys = [];
+  int _frontCardCount = 0;
+  final List<int> _frontCardIndexes = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    reset();
+  }
+
+  void reset() {
+    // add 2 times
+    _randomImageNames.clear();
+    _randomImageNames.addAll(_imageNames);
+    _randomImageNames.addAll(_imageNames);
+
+    // shuffle
+    _randomImageNames.shuffle();
+
+    // create global key
+    _cardKeys.clear();
+    _cardKeys.addAll(_randomImageNames.map((_) => GlobalKey<FlipCardState>()));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Wrap(
+          spacing: 4,
+          runSpacing: 4,
+          children: List.generate(
+            _randomImageNames.length,
+            (index) {
+              if (_randomImageNames[index].isEmpty) {
+                return Container(
+                  width: 100,
+                  height: 150,
+                  color: Colors.transparent,
+                );
+              }
+              return FlipCard(
+                key: _cardKeys[index],
+                onFlip: () {
+                  _frontCardCount++;
+                  _frontCardIndexes.add(index);
+                },
+                onFlipDone: (bool) {
+                  if (_frontCardCount == 2) {
+                    _toggleCardToFront();
+
+                    _checkCardIsEqual();
+
+                    _frontCardCount = 0;
+
+                    if (_frontCardIndexes.length >= 2) {
+                      String firstCardName = _randomImageNames[_frontCardIndexes[0]];
+                      String secondCardName = _randomImageNames[_frontCardIndexes[1]];
+                      if (firstCardName == secondCardName) {
+                        _randomImageNames[_frontCardIndexes[0]] = '';
+                        _randomImageNames[_frontCardIndexes[1]] = '';
+
+                        setState(() {});
+                      }
+                    }
+
+                    _frontCardIndexes.clear();
+                    _frontCardCount = 0;
+                  }
+                },
+                front: Container(
+                  width: 100,
+                  height: 150,
+                  color: Colors.orange,
+                ),
+                back: Container(
+                  width: 100,
+                  height: 150,
+                  child: Image.asset(
+                    _randomImageNames[index],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            },
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          setState(() {
+            reset();
+          });
+        },
+        child: const Icon(Icons.refresh),
+      ),
     );
+  }
+
+  void _toggleCardToFront() {
+    for (var cardKey in _cardKeys) {
+      if (cardKey.currentState == null) return;
+
+      if (!cardKey.currentState!.isFront) {
+        cardKey.currentState!.toggleCard();
+      }
+    }
+  }
+
+  void _checkCardIsEqual() {
+    print('_checkCardIsEqual');
+    print(_frontCardIndexes);
+    if (_frontCardIndexes.length >= 2) {
+      String firstCardName = _randomImageNames[_frontCardIndexes[0]];
+      String secondCardName = _randomImageNames[_frontCardIndexes[1]];
+      if (firstCardName == secondCardName) {
+        _randomImageNames[_frontCardIndexes[0]] = '';
+        _randomImageNames[_frontCardIndexes[1]] = '';
+
+        setState(() {});
+      }
+    }
+
+    _frontCardIndexes.clear();
   }
 }
