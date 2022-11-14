2d969a674086b57c9a5156b9244d9701a1a330f6
jambottle <jaewon.walks@gmail.com>
2022-11-15 05:45:46 +0900

    refactor: Use FlutterGen to automatically generate AssetImage class

--- lib/asset_name.dart
+++
@@ -1,7 +0,0 @@
-class AssetImageName {
-  static String get apple => 'assets/images/apple.jpg';
-  static String get banana => 'assets/images/banana.jpg';
-  static String get kiwi => 'assets/images/kiwi.jpg';
-  static String get orange => 'assets/images/orange.jpg';
-  static String get strawberry => 'assets/images/strawberry.jpg';
-}
