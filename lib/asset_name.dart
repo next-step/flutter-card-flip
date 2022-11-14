79a4c64055a2d283dad44836bbb4aacd866019ce
jambottle <jaewon.walks@gmail.com>
2022-11-14 22:10:43 +0900

    rename: Create an assets/images folder and Move images to the folder

--- lib/asset_name.dart
+++ lib/asset_name.dart
@@ -1,7 +1,7 @@
 class AssetImageName {
-  static String get apple => 'images/apple.jpg';
-  static String get banana => 'images/banana.jpg';
-  static String get kiwi => 'images/kiwi.jpg';
-  static String get orange => 'images/orange.jpg';
-  static String get strawberry => 'images/strawberry.jpg';
+  static String get apple => 'assets/images/apple.jpg';
+  static String get banana => 'assets/images/banana.jpg';
+  static String get kiwi => 'assets/images/kiwi.jpg';
+  static String get orange => 'assets/images/orange.jpg';
+  static String get strawberry => 'assets/images/strawberry.jpg';
 }
